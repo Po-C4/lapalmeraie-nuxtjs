b897063f342aaa8a9d55b65e7b0843024c1e77cb
Po-C4 <leflem.mateo2002@gmail.com>
2021-01-07 04:59:51 +0100

    Added error page

---
+++ layouts/error.vue
@@ -0,0 +1,49 @@
+<template>
+  <b-container fluid class="bg px-0 text-white">
+    <b-row class="bg-overlay">
+      <b-col>
+        <h1 class="display-3 w-100">
+          Erreur {{ error.statusCode }}
+        </h1>
+        <p v-if="error.statusCode === 403" class="lead mt-3 w-100">
+          L'accès à cette page n'est pas autorisé !
+        </p>
+        <p v-else class="lead mt-3 w-100">
+          {{ error.message }}
+        </p>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+export default {
+  props: {
+    error: {
+      type: Object,
+      default: null,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.bg {
+  height: 100vh;
+  width: 100%;
+  background-image: url('~assets/bg-error.png');
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  padding: 0;
+}
+.bg-overlay {
+  display: flex;
+  height: 100vh;
+  width: 100%;
+  margin: 0;
+  text-align: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.5);
+}
+</style>
